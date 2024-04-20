cef9b04aa798ac12e275c651bf232a81b7166ddb
gichanlee <gichanlee@icloud.com>
2024-04-20 14:03:54 +0900

    Implement handleNickCommand for nickname management
    This commit introduces the handleNickCommand function to manage nickname changes within the IRC server. It checks if the desired nickname is already in use and broadcasts the change to all clients. This update ensures the server adheres to IRC protocol standards for nickname management and enhances user synchronization.

--- IRCServer.hpp
+++ IRCServer.hpp
@@ -10,6 +10,7 @@
 #include <vector>
 #include <poll.h>
 #include <queue>
+#include <map>
 
 
 class IRCServer {
@@ -23,11 +24,13 @@ public:
     void handleClientData(int clientSocket);
     void handleClientWrite(int clientSocket);
     size_t findClientSocketIndex(int clientSocket);
+    void handleNickCommand(int clientSocket, const std::string& nickname);
 
 private:
     ServerSocket serverSocket;
     std::vector<struct pollfd> fds;
     std::vector<std::queue<std::string> > messageQueues;
+    std::map<int, std::string> clientNicknames;
 };
 
 void IRCServer::run() {
@@ -67,9 +70,17 @@ void IRCServer::acceptNewClients() {
         if (clientSocket >= 0) {
             std::cout << "New client connected, socket fd: " << clientSocket << std::endl;
 
+            // Prepare the client's pollfd structure
             struct pollfd client_fd = {clientSocket, POLLIN | POLLOUT, 0};
             fds.push_back(client_fd);
+
+            // Create a message queue for the new client
             messageQueues.push_back(std::queue<std::string>());
+
+            // Send a welcome message to the new client
+            std::string welcomeMessage = ":Server NOTICE AUTH :Welcome to the IRC Server!\r\n";
+            messageQueues.back().push(welcomeMessage); // Add the welcome message to the new client's queue
+            fds.back().events |= POLLOUT; // Ensure the POLLOUT event is set to send the message
 
             std::cout << "Client added. fds size: " << fds.size() << ", messageQueues size: " << messageQueues.size() << std::endl;
         } else {
@@ -77,7 +88,6 @@ void IRCServer::acceptNewClients() {
         }
     }
 }
-
 
 void IRCServer::handleClientEvents() {
     for (size_t i = 1; i < fds.size(); ++i) {
@@ -108,14 +118,14 @@ void IRCServer::handleClientData(int clientSocket) {
     ssize_t bytesRead = read(clientSocket, buffer, sizeof(buffer) - 1);
 
     if (bytesRead > 0) {
+        buffer[bytesRead] = '\0';  // Ensure null-termination of the string
         std::cout << "Received data from client: " << buffer << std::endl;
-        // TODO: send received message to all clients
-        for (size_t i = 1; i < fds.size(); ++i) {
-            if (fds[i].fd != serverSocket.getFD()) {
-                messageQueues[i-1].push(std::string(buffer));
-                fds[i].events |= POLLOUT;
-            }
+        std::string message(buffer);
+        if (message.rfind("NICK ", 0) == 0) {
+            std::string nickname = message.substr(5);
+            handleNickCommand(clientSocket, nickname);
         }
+
     } else if (bytesRead == 0) {
         std::cout << "Client disconnected." << std::endl;
         size_t index = findClientSocketIndex(clientSocket);
@@ -130,12 +140,46 @@ void IRCServer::handleClientData(int clientSocket) {
     }
 }
 
+void IRCServer::handleNickCommand(int clientSocket, const std::string &nickname) {
+    // Check if the nickname is already in use
+    std::map<int, std::string>::iterator it;
+    for (it = clientNicknames.begin(); it != clientNicknames.end(); ++it) {
+        if (it->second == nickname) {
+            // Nickname in use, send an error message back to the client
+            std::string errMsg = ":Server 433 * " + nickname + " :Nickname is already in use\r\n";
+            size_t clientIndex = findClientSocketIndex(clientSocket);
+            if (clientIndex != std::string::npos) {
+                messageQueues[clientIndex].push(errMsg);
+                fds[clientIndex + 1].events |= POLLOUT; // Set POLLOUT to send the error message
+            }
+            return;
+        }
+    }
+
+    // If the nickname is not in use, update the client's nickname
+    clientNicknames[clientSocket] = nickname;
+    std::string nickChangeMsg = ":Server NOTICE AUTH : NICK changed to " + nickname + "\r\n";
+
+    // Broadcast the nickname change to all clients
+    for (size_t i = 1; i < fds.size(); ++i) {
+        if (fds[i].fd != serverSocket.getFD()) { // Ensure it's a client socket
+            messageQueues[i-1].push(nickChangeMsg);
+            fds[i].events |= POLLOUT; // Set POLLOUT to trigger sending the message
+        }
+    }
+    std::cout << "Nickname change for client " << clientSocket << " to " << nickname << std::endl;
+}
+
+
+
 void IRCServer::handleClientWrite(int clientSocket) {
     size_t index = findClientSocketIndex(clientSocket);
+    std::cout << "Attempting to write to client at index: " << index << std::endl;
 
     if (index != std::string::npos && !messageQueues[index].empty()) {
         const std::string& message = messageQueues[index].front();
         ssize_t bytesWritten = write(clientSocket, message.c_str(), message.length());
+        std::cout << "Trying to send message: " << message << std::endl;
 
         if (bytesWritten > 0) {
             std::cout << "Sent message to client." << std::endl;
